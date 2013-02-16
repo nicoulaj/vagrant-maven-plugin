c2a86f18c55f00576502a54278ca3cd35ca934d1
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2013-02-16 11:08:51 +0100

    Add destroy goal

---
+++ src/main/java/net/nicoulaj/maven/plugins/vagrant/DestroyMojo.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2013 vagrant-maven-plugin contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.nicoulaj.maven.plugins.vagrant;
+
+import de.saumya.mojo.ruby.script.ScriptException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Invokes Vagrant {@code destroy} command.
+ *
+ * @author <a href="http://github.com/nicoulaj">Julien Nicoulaud</a>
+ * @goal destroy
+ * @since 1.0
+ */
+@SuppressWarnings("unused")
+public final class DestroyMojo extends AbstractVagrantMojo {
+
+    /** Mojo/Vagrant command name. */
+    public static final String NAME = "destroy";
+
+    /**
+     * VM name.
+     *
+     * @parameter
+     */
+    protected String vm;
+
+    /**
+     * Force shut down.
+     *
+     * @parameter default-value="false"
+     */
+    protected boolean force;
+
+    @Override
+    protected void doExecute() throws IOException, ScriptException {
+
+        final List<String> args = new ArrayList<String>();
+
+        args.add(NAME);
+
+        if (vm != null)
+            args.add(vm);
+
+        if (force)
+            args.add("--force");
+
+        cli(args);
+    }
+}
